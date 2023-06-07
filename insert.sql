e6efb2cafe0bf05948640d1ab1243eda7a23f5fd
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-06-07 21:34:10 +0545

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMER (first_name,last_name,email,company,street,
+					  city,state,zip,phone,birth_date,sex,date_entered) 
+VALUES('anish','mainali','anishmainali@gmail.com','BP','347 surunga','jhapa',
+	  'ME','30044','9817911502','1999-03-01','M',current_timestamp);
